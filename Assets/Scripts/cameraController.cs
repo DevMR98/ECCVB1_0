bf02ed9c331109734622db1ca9d4d0794430b0de
Miguel Rodríguez <miguel.rguez9899@gmail.com>
2022-08-30 09:06:57 -0500

    Initial commit

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public float Yaxis;
+    public float Xaxis;
+    public float rotationSensitivity = 0.5f;
+    public bool enableMobileInput = false;
+    public FixedTouchField touchField;
+    public Transform target;
+
+    void Update()
+    {
+
+        if (enableMobileInput)
+        {
+            Yaxis += touchField.TouchDist.x * rotationSensitivity;
+            Xaxis -= touchField.TouchDist.y * rotationSensitivity;
+
+        }
+        else
+        {
+            Yaxis += Input.GetAxis("Mouse X") * rotationSensitivity;
+            Xaxis -= Input.GetAxis("Mouse Y") * rotationSensitivity;
+
+        }
+
+        Vector3 targetRotation = new Vector3(Xaxis, Yaxis);
+        transform.eulerAngles = targetRotation;
+        transform.position = target.position - transform.forward * 2f;
+    }
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ECCVB1_0
+ Juego educativo acerca de El Cid Campeador
