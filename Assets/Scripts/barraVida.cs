d15e9e82c6caa9bf1939b30b312386710068468c
Miguel Rodríguez <miguel.rguez9899@gmail.com>
2022-09-17 17:22:06 -0500

    minimapa y pruebas de barra de vida en el personaje y npc

---
+++ Assets/Scripts/barraVida.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class barraVida : MonoBehaviour
+{
+
+    public Image barraDeVida;
+    public int vidaMax;
+    public float vidaActual;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaActual=vidaMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        revisarVida();
+
+        if(vidaActual<=0){
+            gameObject.SetActive(false);
+        }
+        
+    }
+
+    public void revisarVida(){
+        barraDeVida.fillAmount=vidaActual/vidaMax;
+
+    }
+}
