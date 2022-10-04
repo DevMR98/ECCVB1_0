eb0563100c7003896406a95d8c04ddd24e7a6812
Miguel Rodríguez <miguel.rguez9899@gmail.com>
2022-10-03 21:57:26 -0500

    solucion del bug de salto al inicio en móviles y inclusión de un combo de ataque
    Aun falta cambiar algunas animaciones de ataque y movimiento

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -17,48 +17,66 @@ public class characterController : MonoBehaviour
     //Variable que definira si se juega en dispositivo movil o pc
     [HideInInspector]
     public ControllerType controller;
+    public bool isAttack;
+    public bool forwardAlone;
+    public float kickImpulse=10f;
+    int cantidad_click; 
+    bool puedo_dar_click; 
 
     // Start is called before the first frame update
     void Start()
     {
         anim=GetComponent<Animator>();
         canJump=false;
+        cantidad_click = 0;
+        puedo_dar_click = true;
        
         
     }
-
     // Update is called once per frame
     void Update()
     {
-
-        
         //salto
+        //esto no tiene sentido pero jala :v
+        if(controller==ControllerType.PC || controller==ControllerType.MOBILE){
+            jump();
+            attack();
+            //attackSword();
+        }
+
+        /*Esto deberia ser lo correcto pero no jala :v
         if(controller==ControllerType.PC){
             jump();
-        }else{
+            attack();
+            attackSword();
+        }else if(controller==ControllerType.MOBILE){
             jumpMobile();
-        }
-        
-        
+            attackSwordMobile();
+        }*/
         
     }
-
     void FixedUpdate() {
-        move();
+        if(!isAttack){
+            move();
+        }        
+    //code by attack
+        if(forwardAlone){
+            rb.velocity=transform.forward*kickImpulse;
+        }
     }
-
     public void jump(){
         if(canJump){
-            if(Input.GetButtonDown("Jump")){
-            anim.SetBool("jump",true);
-            rb.AddForce(new Vector3(0,jumpForce,0),ForceMode.Impulse);
+            if(!isAttack){
+                if(Input.GetButtonDown("Jump")){
+                anim.SetBool("jump",true);
+                rb.AddForce(new Vector3(0,jumpForce,0),ForceMode.Impulse);
             }
+            }   
             anim.SetBool("isOnFloor",true);    
         }else{
             isFallen();
         }
     }
-
     public void jumpMobile(){
          if(canJump){
             anim.SetBool("jump",true);
@@ -67,7 +85,6 @@ public class characterController : MonoBehaviour
         }else{
             isFallen();
         }
-
     }
 
     public void isFallen(){
@@ -103,32 +120,82 @@ public class characterController : MonoBehaviour
             //control de movimiento del personaje
             transform.Translate(0,0,y*Time.deltaTime*movementVelocity);
         }
-
-        
-       
-
-    /*
-        //control de la rotacion
-        transform.Rotate(0,x*Time.deltaTime*rotationVelocity,0);
-        //control de movimiento del personaje
-        transform.Translate(0,0,y*Time.deltaTime*movementVelocity); 
-
-        
-
-
-        //asignando el movimeinto al animator
-        anim.SetFloat("velX",x);
-        anim.SetFloat("velY",y);
-*/
+    }
+    public void attack(){
+        if(Input.GetKeyDown(KeyCode.Return) && canJump && !isAttack){
+            anim.SetTrigger("ataque");
+            isAttack=true;
+        }
+    }
+    public  void leaveAtack(){
+        isAttack=false;
+        forwardAlone=false;
     }
 
-    
+    public void forward_Alone(){
+        forwardAlone=true;
+    }
+    public void leaveAlone(){
+        forwardAlone=false;
+    }
 
-    
+//***************************COMBO DE ATAQUE*****************************************************
+     void Iniciar_combo()
+    {
+        if (puedo_dar_click)
+        {
+            cantidad_click++;
+        }
 
+        if (cantidad_click == 1)
+        {
+            anim.SetInteger("attackSword", 1);
+        }
+    }
 
+    public void Verificar_combo()
+    {
 
-   
+        puedo_dar_click = false;
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 1") && cantidad_click == 1)
+        {
+            anim.SetInteger("attackSword", 0);
+            puedo_dar_click = true;
+            cantidad_click = 0;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 1") && cantidad_click >= 2)
+        {       
+            anim.SetInteger("attackSword", 2);
+            puedo_dar_click = true;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && cantidad_click == 2)
+        {       
+            anim.SetInteger("attackSword", 0);
+            puedo_dar_click = true;
+            cantidad_click = 0;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && cantidad_click >= 3)
+        {       
+            anim.SetInteger("attackSword", 3);
+            puedo_dar_click = true;
+        }
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 3"))
+        {      
+            anim.SetInteger("attackSword", 0);
+            puedo_dar_click = true;
+            cantidad_click = 0;
+        }
+    }
+
+    public void attackSword(){
+        if (Input.GetKeyDown(KeyCode.Backspace)) { Iniciar_combo(); }
+    }
+
+    public void attackSwordMobile(){
+        Iniciar_combo();
+    }
+
 
 
 }
