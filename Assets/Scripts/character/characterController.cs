eb9da4cc68bc6a909a1ee9f90b51718dcc5d836d
Miguel Rodríguez <miguel.rguez9899@gmail.com>
2022-10-06 22:59:54 -0500

    se creo el comportamiento del enemigo
    el enemigo busca el personaje mediante su rango de vision y lopersigue
    el personaje ataca al jugador en un rango especifico
    aun falta añadir las mecanicas de la barra de vida y mejorar mas el sistema de combate

--- Assets/Scripts/character/characterController.cs
+++ Assets/Scripts/character/characterController.cs
@@ -10,16 +10,9 @@ public class characterController : MonoBehaviour
     //tomando porpiedades del joystick touch
     public FixedJoystick joystick;
     float x,y;
-    //Comprobacion salto
-    public bool  canJump;
-    public Rigidbody rb;
-    public float jumpForce=8f;
     //Variable que definira si se juega en dispositivo movil o pc
     [HideInInspector]
     public ControllerType controller;
-    public bool isAttack;
-    public bool forwardAlone;
-    public float kickImpulse=10f;
     int cantidad_click; 
     bool puedo_dar_click; 
 
@@ -27,7 +20,6 @@ public class characterController : MonoBehaviour
     void Start()
     {
         anim=GetComponent<Animator>();
-        canJump=false;
         cantidad_click = 0;
         puedo_dar_click = true;
        
@@ -36,62 +28,14 @@ public class characterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //salto
-        //esto no tiene sentido pero jala :v
-        if(controller==ControllerType.PC || controller==ControllerType.MOBILE){
-            jump();
-            attack();
-            //attackSword();
-        }
-
-        /*Esto deberia ser lo correcto pero no jala :v
         if(controller==ControllerType.PC){
-            jump();
-            attack();
             attackSword();
-        }else if(controller==ControllerType.MOBILE){
-            jumpMobile();
-            attackSwordMobile();
-        }*/
-        
+        }
     }
     void FixedUpdate() {
-        if(!isAttack){
             move();
-        }        
-    //code by attack
-        if(forwardAlone){
-            rb.velocity=transform.forward*kickImpulse;
-        }
     }
-    public void jump(){
-        if(canJump){
-            if(!isAttack){
-                if(Input.GetButtonDown("Jump")){
-                anim.SetBool("jump",true);
-                rb.AddForce(new Vector3(0,jumpForce,0),ForceMode.Impulse);
-            }
-            }   
-            anim.SetBool("isOnFloor",true);    
-        }else{
-            isFallen();
-        }
-    }
-    public void jumpMobile(){
-         if(canJump){
-            anim.SetBool("jump",true);
-            rb.AddForce(new Vector3(0,jumpForce,0),ForceMode.Impulse);
-            anim.SetBool("isOnFloor",true);    
-        }else{
-            isFallen();
-        }
-    }
-
-    public void isFallen(){
-        anim.SetBool("isOnFloor",false);
-        anim.SetBool("jump",false);
-    }
-
+     
     public void move(){
 
         if(controller==ControllerType.PC){
@@ -121,67 +65,43 @@ public class characterController : MonoBehaviour
             transform.Translate(0,0,y*Time.deltaTime*movementVelocity);
         }
     }
-    public void attack(){
-        if(Input.GetKeyDown(KeyCode.Return) && canJump && !isAttack){
-            anim.SetTrigger("ataque");
-            isAttack=true;
-        }
-    }
-    public  void leaveAtack(){
-        isAttack=false;
-        forwardAlone=false;
-    }
-
-    public void forward_Alone(){
-        forwardAlone=true;
-    }
-    public void leaveAlone(){
-        forwardAlone=false;
-    }
+   
+    
 
 //***************************COMBO DE ATAQUE*****************************************************
-     void Iniciar_combo()
-    {
-        if (puedo_dar_click)
-        {
+     void Iniciar_combo(){
+        if (puedo_dar_click){
             cantidad_click++;
         }
 
-        if (cantidad_click == 1)
-        {
+        if (cantidad_click == 1){
             anim.SetInteger("attackSword", 1);
         }
     }
 
-    public void Verificar_combo()
-    {
+    public void Verificar_combo(){
 
         puedo_dar_click = false;
 
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 1") && cantidad_click == 1)
-        {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 1") && cantidad_click == 1){
             anim.SetInteger("attackSword", 0);
             puedo_dar_click = true;
             cantidad_click = 0;
         }
-        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 1") && cantidad_click >= 2)
-        {       
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 1") && cantidad_click >= 2){       
             anim.SetInteger("attackSword", 2);
             puedo_dar_click = true;
         }
-        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && cantidad_click == 2)
-        {       
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && cantidad_click == 2){       
             anim.SetInteger("attackSword", 0);
             puedo_dar_click = true;
             cantidad_click = 0;
         }
-        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && cantidad_click >= 3)
-        {       
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && cantidad_click >= 3){       
             anim.SetInteger("attackSword", 3);
             puedo_dar_click = true;
         }
-        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 3"))
-        {      
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 3")){      
             anim.SetInteger("attackSword", 0);
             puedo_dar_click = true;
             cantidad_click = 0;
@@ -191,9 +111,15 @@ public class characterController : MonoBehaviour
     public void attackSword(){
         if (Input.GetKeyDown(KeyCode.Backspace)) { Iniciar_combo(); }
     }
-
     public void attackSwordMobile(){
         Iniciar_combo();
+    }
+
+    public void OnTriggerEnter(Collider coll){
+        if(coll.CompareTag("arma")){
+            print("prueba daño");
+        }
+        
     }
 
 
